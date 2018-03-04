f47c327546ac7bef93c9cf92f2ee5b911b8233cb
KristofferK <kristoffer@minlejebolig.dk>
2018-03-04 21:00:56 +0100

    Add functionality to read content of XML file

---
+++ EIP_MSMQ_SplitResequenceAggregate/A_CheckIn/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_CheckIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static string GetContentOfXmlFile()
+        {
+            var projectFolder = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            var path = projectFolder + "\\xml-file\\FlightDetailsInfoResponse.xml";
+            return File.ReadAllText(path);
+        }
+    }
+}
---
+++ EIP_MSMQ_SplitResequenceAggregate/ClassLibrary/MessageQueueGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public static class MessageQueueGenerator
+    {
+        public static string AirlineToAirportRequestChannel { get; private set; } = @".\Private$\AirlineToAirportChannel";
+        public static string AirportToAirlineReplyChannel { get; private set; } = @".\Private$\AirportToAirlineReplyChannel";
+
+        public static MessageQueue GenerateMessageQueue(string messageQueueName)
+        {
+            if (!MessageQueue.Exists(messageQueueName))
+            {
+                MessageQueue.Create(messageQueueName);
+            }
+            return new MessageQueue(messageQueueName)
+            {
+                Label = "I'm located at " + messageQueueName
+            };
+        }
+    }
+}
